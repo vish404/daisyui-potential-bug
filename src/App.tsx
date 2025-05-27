dd543b27ced268ae0aed34300f3d5fe9e3a0cc7e
Vish <visanimator@yahoo.com>
2025-05-27 10:46:15 +0300

    Calendar issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-function App() {
+import { DayPicker } from "react-day-picker";
+
+export default function App() {
   return (
     <>
-      <div>
-        <div className="divider"></div>
+      <button
+        popoverTarget="rdp-popover"
+        className="input input-border"
+        style={{ anchorName: "--rdp" } as React.CSSProperties}
+      >
+        {"Pick a date"}
+      </button>
+      <div
+        popover="auto"
+        id="rdp-popover"
+        className="dropdown"
+        style={{ positionAnchor: "--rdp" } as React.CSSProperties}
+      >
+        <DayPicker className="react-day-picker" mode="range" />
       </div>
     </>
   );
 }
-
-export default App;
